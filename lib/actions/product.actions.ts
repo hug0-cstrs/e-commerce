b223b6ec806e1c7ad338afd39830a5f7bd34d722
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-10 08:00:34 +0000

    Refactor product.actions.ts for better code organization

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,20 @@
-'use server'
+"use server";
 
-import { desc } from 'drizzle-orm'
+import { desc, eq } from "drizzle-orm";
 
-import db from '@/db/drizzle'
-import { products } from '@/db/schema'
-
-
+import db from "@/db/drizzle";
+import { products } from "@/db/schema";
 
 export async function getLatestProducts() {
   const data = await db.query.products.findMany({
     orderBy: [desc(products.createdAt)],
     limit: 4,
-  })
-  return data
+  });
+  return data;
+}
+
+export async function getProductBySlug(slug: string) {
+  return await db.query.products.findFirst({
+    where: eq(products.slug, slug),
+  });
 }
