160e9270fceb8429050229020573ef6f89c53244
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-10 07:44:23 +0000

    add new file

---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Maurice e-commerce'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An e-commerce website for Maurice built with Next.js, Postgres, Shadcn'
---
+++ types/index.ts
@@ -0,0 +1,6 @@
+
+import { products } from '@/db/schema';
+import { InferSelectModel } from 'drizzle-orm';
+
+// PRODUCTS
+export type Product = InferSelectModel<typeof products>
