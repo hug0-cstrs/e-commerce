7c73943cd3276a6d981f9e5eb91b5fda6d3d420c
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-11 13:55:11 +0000

    Update types/index.ts to include Cart and CartItem types

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,11 @@
-
-import { products } from '@/db/schema';
-import { InferSelectModel } from 'drizzle-orm';
+import { carts, products } from "@/db/schema";
+import { cartItemSchema } from "@/lib/validators";
+import { InferSelectModel } from "drizzle-orm";
+import { z } from "zod";
 
 // PRODUCTS
-export type Product = InferSelectModel<typeof products>
+export type Product = InferSelectModel<typeof products>;
+
+// CART
+export type Cart = InferSelectModel<typeof carts>;
+export type CartItem = z.infer<typeof cartItemSchema>;
