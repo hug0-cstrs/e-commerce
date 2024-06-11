9275b317a90f79a0ab2df34e3a744755644115d1
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-11 22:16:19 +0000

    Update validators.ts with cartItemSchema and shippingAddressSchema

--- lib/validators.ts
+++ lib/validators.ts
@@ -21,17 +21,27 @@ export const signUpFormSchema = z
     path: ["confirmPassword"],
   });
 
-  // CART
+// CART
 export const cartItemSchema = z.object({
-    productId: z.string().min(1, 'Product is required'),
-    name: z.string().min(1, 'Name is required'),
-    slug: z.string().min(1, 'Slug is required'),
-    qty: z.number().int().nonnegative('Quantity must be a non-negative number'),
-    image: z.string().min(1, 'Image is required'),
-    price: z
-      .number()
-      .refine(
-        (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
-        'Price must have exactly two decimal places (e.g., 49.99)'
-      ),
-  })
+  productId: z.string().min(1, "Product is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  qty: z.number().int().nonnegative("Quantity must be a non-negative number"),
+  image: z.string().min(1, "Image is required"),
+  price: z
+    .number()
+    .refine(
+      (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
+      "Price must have exactly two decimal places (e.g., 49.99)"
+    ),
+});
+
+export const shippingAddressSchema = z.object({
+  fullName: z.string().min(3, "Name must be at least 3 characters"),
+  streetAddress: z.string().min(3, "Address must be at least 3 characters"),
+  city: z.string().min(3, "city must be at least 3 characters"),
+  postalCode: z.string().min(3, "Postal code must be at least 3 characters"),
+  country: z.string().min(3, "Country must be at least 3 characters"),
+  lat: z.number().optional(),
+  lng: z.number().optional(),
+});
