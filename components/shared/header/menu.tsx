6503c88fde26d4dfa6a49bb567157ca20994c234
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-11 08:52:20 +0000

    Add menu component to header

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import UserButton from "./user-button";
+
+const Menu = () => {
+  return (
+    <>
+      <div className="flex justify-end gap-3">
+        <nav className="md:flex hidden w-full max-w-xs gap-1">
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+                <ShoppingCart />
+                Cart
+            </Link>
+        </Button>
+        <UserButton />
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default Menu;
