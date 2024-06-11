a8a2188259458f7866dbdc087b5b9c517e480bb2
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-11 22:16:31 +0000

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,19 @@
 "use server";
 
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import db from "@/db/drizzle";
 import { users } from "@/db/schema";
+import { ShippingAdress } from "@/types";
 import { hashSync } from "bcrypt-ts-edge";
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 import { isRedirectError } from "next/dist/client/components/redirect";
 import { formatError } from "../utils";
-import { signInFormSchema, signUpFormSchema } from "../validators";
+import {
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../validators";
 
 // USER
 export async function signUp(prevState: unknown, formData: FormData) {
@@ -65,3 +72,31 @@ export async function signInWithCredentials(
 export const SignOut = async () => {
   await signOut();
 };
+
+export async function getUserById(userId: string) {
+  const user = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.id, userId),
+  });
+  if (!user) throw new Error("User not found");
+  return user;
+}
+
+export async function updateUserAddress(data: ShippingAdress) {
+  try {
+    const session = await auth();
+    const currentUser = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.id, session?.user.id!),
+    });
+    if (!currentUser) throw new Error("User not found");
+
+    const address = shippingAddressSchema.parse(data);
+    await db.update(users).set({ address }).where(eq(users.id, currentUser.id));
+    revalidatePath("/place-order");
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
