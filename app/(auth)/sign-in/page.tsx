b92932aff4fb042a33b626840b8a8ae5ab458c31
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-12 23:45:59 +0000

    Update logo source in sign-in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -37,7 +37,7 @@ export default async function SignIn({
         <CardHeader className="space-y-4">
           <Link href="/" className="flex-center">
             <Image
-              src="/assets/icons/logo.svg"
+              src="/assets/icons/holberton_logo.svg"
               width={100}
               height={100}
               alt={`${APP_NAME} logo`}
