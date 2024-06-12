eed2f316feda6c7bfa5666b1723cb14fcca2b71a
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-12 23:49:58 +0000

    Add user profile, orders, and admin pages to header dropdown menu

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -43,6 +43,26 @@ export default async function UserButton() {
             </div>
           </DropdownMenuLabel>
 
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/profile">
+              Profile
+            </Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/orders">
+              Order History
+            </Link>
+          </DropdownMenuItem>
+
+          {session.user.role === 'admin' && (
+            <DropdownMenuItem>
+              <Link className="w-full" href="/admin/overview">
+                Admin
+              </Link>
+            </DropdownMenuItem>
+          )}
+
           <DropdownMenuItem className="p-0 mb-1">
             <form action={SignOut} className="w-full">
               <Button
