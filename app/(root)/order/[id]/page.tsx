e6d71631b6eb842f0f3d9dfe6f7d454ee41766d0
hug0-cstrs <hugo.casteras@outlook.fr>
2024-06-13 07:55:07 +0000

    Update OrderDetailsPage to use authenticated user role for isAdmin check

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,28 +1,31 @@
-import { getOrderById } from '@/lib/actions/order.actions'
-import { APP_NAME } from '@/lib/constants'
-import { notFound } from 'next/navigation'
-import OrderDetailsForm from './order-details-form'
+import { auth } from "@/auth";
+import { getOrderById } from "@/lib/actions/order.actions";
+import { APP_NAME } from "@/lib/constants";
+import { notFound } from "next/navigation";
+import OrderDetailsForm from "./order-details-form";
 
 export const metadata = {
   title: `Order Details - ${APP_NAME}`,
-}
+};
 
 const OrderDetailsPage = async ({
   params: { id },
 }: {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }) => {
-  const order = await getOrderById(id)
-  if (!order) notFound()
-
+  const session = await auth();
+  const order = await getOrderById(id);
+  if (!order) notFound();
+  order.user;
   return (
     <OrderDetailsForm
       order={order}
-      paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+      paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      isAdmin={session?.user.role === "admin" || false}
     />
-  )
-}
+  );
+};
 
 export default OrderDetailsPage;
